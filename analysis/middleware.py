6cf1c065d0c67d60e2181bb6d283245073d1cb1c
bubblevisions <bubblevision166@gmail.com>
2026-05-02 23:01:03 +0800

    Update middleware.py

--- analysis/middleware.py
+++ analysis/middleware.py
@@ -1,1 +1,14 @@
 
+from django.utils.cache import add_never_cache_headers
+
+class DisableClientSideCachingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        # We only apply this to logged-in users to prevent 
+        # private dashboards from being cached in the browser.
+        if request.user.is_authenticated:
+            add_never_cache_headers(response)
+        return response
